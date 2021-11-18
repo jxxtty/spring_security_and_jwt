1f3ce6b66858b30ba17d10045acfacd837c8ebc2
jxxtty <jaehee941106@gmail.com>
2021-11-18 17:39:37 +0900

    초안

---
+++ src/main/java/com/example/securityAndJWT/SecurityAndJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityAndJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAndJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAndJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityAndJWT/SecurityAndJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityAndJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAndJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
